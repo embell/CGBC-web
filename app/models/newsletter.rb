19e71a91e14d4ee43d7c8f7fc06ef5a281a3fd13
Ethan Bell <emb1234@gmail.com>
2016-06-02 22:23:04 -0400

    Add validations to Newsletter model

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,4 +1,8 @@
 class Newsletter < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :date
+  validates_presence_of :document
+
   mount_uploader :document, NewsletterUploader
 
   def month
