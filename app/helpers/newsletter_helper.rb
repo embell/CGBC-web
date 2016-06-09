b43937c81b3643908ca1301196ab2a31cf2e8055
embell <emb1234@gmail.com>
2016-06-09 12:05:50 -0400

    Fix style issue on case statement in Newsletter helper

--- app/helpers/newsletter_helper.rb
+++ app/helpers/newsletter_helper.rb
@@ -4,34 +4,34 @@ module NewsletterHelper
   def get_newsletter_name(date)
     month = date.month
     month = case month
-    when 1
-      'Jan'
-    when 2
-      'Feb'
-    when 3
-      'Mar'
-    when 4
-      'Apr'
-    when 5
-      'May'
-    when 6
-      'Jun'
-    when 7
-      'Jul'
-    when 8
-      'Aug'
-    when 9
-      'Sep'
-    when 10
-      'Oct'
-    when 11
-      'Nov'
-    when 12
-      'Dec'
-    else
-      ''
-    end
+            when 1
+              'Jan'
+            when 2
+              'Feb'
+            when 3
+              'Mar'
+            when 4
+              'Apr'
+            when 5
+              'May'
+            when 6
+              'Jun'
+            when 7
+              'Jul'
+            when 8
+              'Aug'
+            when 9
+              'Sep'
+            when 10
+              'Oct'
+            when 11
+              'Nov'
+            when 12
+              'Dec'
+            else
+              ''
+            end
 
-    "#{month} #{date.year.to_s}"
+    "#{month} #{date.year}"
   end
 end
