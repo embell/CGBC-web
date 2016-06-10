cdbd2c248ac34d0defab11d575ac8a1f4b84eacd
Ethan Bell <emb1234@gmail.com>
2016-06-09 23:14:38 -0400

    Add tests for get_page_title helper
    Tests make sure the correct title is returned when passing in empty, nil, or something else. The name of this helper also changed from previous commit, get_page_title should be a little more descriptive than just get_title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # Use this module for utility methods useful throughout the application.
 module ApplicationHelper
-  # This helper gets the title for a page. Combines passed in info (if any) with base title, "CGBC"
-  def get_title(sub_title)
+  # This helper gets the title for a page. Combines passed info (if any) with base title, "CGBC"
+  def get_page_title(sub_title)
     base_title = "CGBC"
-    sub_title ? "#{base_title} - #{sub_title}" : base_title
+    sub_title.nil? || sub_title.empty? ? base_title : "#{base_title} - #{sub_title}" 
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# Test any helper methods related to the overall application
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "get_page_title" do
+    it "gets correct page title when given non-empty input" do
+      title = get_page_title("Home")
+      expect(title).to eq("CGBC - Home")
+    end
+
+    it "gets correct page title when given non-truthy input" do
+      title = get_page_title(nil)
+      expect(title).to eq("CGBC")
+    end
+
+    it "gets correct page title when given empty input" do
+      title = get_page_title("")
+      expect(title).to eq("CGBC")
+    end
+  end
+end
