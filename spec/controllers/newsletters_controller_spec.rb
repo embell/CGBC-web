4a2182ca3c225fd146a364c680926613c0188767
Ethan Bell <emb1234@gmail.com>
2016-07-10 15:56:37 -0400

    Add more tests for Newsletter creation

--- spec/controllers/newsletters_controller_spec.rb
+++ spec/controllers/newsletters_controller_spec.rb
@@ -8,15 +8,52 @@ RSpec.describe NewslettersController, type: :controller do
     end
   end
 
-  pending "Adding new newsletters only works when logged in with correct permission"
-  #describe '#create' do
-    #describe 'logged off' do
-      ## Try to create it, but fail
-    #end
+  describe 'POST #create' do
+    context 'when logged off' do
+      it 'redirects' do
+        post :create
 
-    #describe 'logged on' do
-      #login_with_permissions(["newsletter"])
-      ## Then create it
-    #end
-  #end
+        expect(response.status).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when logged on without newsletter permission' do
+      login_with_permissions(["sermons"])
+
+      it 'will not upload a new record' do
+        initial_newsletter_count = Newsletter.count
+        post :create, newsletter: { date: Time.now, document: fixture_file_upload('test.txt') }
+
+        expect(flash[:error]).to match(/You do not have permission to edit Newsletters/)
+        expect(Newsletter.count).to eq(initial_newsletter_count)
+      end
+    end
+
+    context 'when logged on with newsletter permission' do
+      login_with_permissions(["newsletters"])
+
+      context 'and using valid parameters' do
+        it 'successfully creates a new record' do
+          initial_newsletter_count = Newsletter.count
+          post :create, newsletter: { date: Time.now, document: fixture_file_upload('test.txt') }
+
+          # Check error messages first, so that failing tests will show them
+          expect(flash[:error]).to eq(nil)
+          expect(Newsletter.count).to eq(initial_newsletter_count + 1)
+        end
+
+        it 'redirects to Newsletter index' do
+          post :create, newsletter: { date: Time.now, document: fixture_file_upload('test.txt') }
+          expect(response.status).to redirect_to(:newsletter)
+        end
+      end
+
+      context 'with invalid parameters' do
+        it 'causes an error' do
+          bypass_rescue
+          expect { post :create }.to raise_error(ActionController::ParameterMissing)
+        end
+      end
+    end
+  end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,10 +2,10 @@ module ControllerMacros
   def login_with_permissions(permissions)
     before (:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = User.create!({email: "test@example.com",
-                           password: "password",
-                           password_confirmation: "password",
-                           permissions: permissions})
+      user = User.create!({ email: "test@example.com",
+                            password: "password",
+                            password_confirmation: "password",
+                            permissions: permissions })
       sign_in user
     end
   end
