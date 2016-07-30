40e0d9d3e26496c273dadbd303e9f692f4999bc8
Ethan Bell <emb1234@gmail.com>
2016-07-30 13:17:40 -0400

    Add pending tests for Sermon search

---
+++ spec/models/sermon_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "Sermons search should find all sermons by speaker" 
+  pending "Sermons search should find all sermons by title" 
+  pending "Sermons search should find all sermons by year" 
+  pending "Sermons search should find all sermons by month" 
+  pending "Sermons search should find all sermons when combining all criteria" 
+end
