b2f797447f31c8c1f3946a5519c5a7c1e6037372
embell <emb1234@gmail.com>
2016-08-17 10:00:06 -0400

    Suppress email errors in dev, correct redirect

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,6 +1,6 @@
 class MailController < ApplicationController
   def send_contact_mail
     ContactMailer.contact_email(params[:name], params[:email], params[:comments]).deliver 
-    redirect_to 'contact', notice: 'Your message has been submitted. Thanks for your feedback.'
+    redirect_to '/contact', notice: 'Your message has been submitted. Thanks for your feedback.'
   end
 end
