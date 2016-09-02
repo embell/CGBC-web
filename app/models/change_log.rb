2499427450b205fa5a2cc03f3f3c301f410d9669
Ethan Bell <emb1234@gmail.com>
2016-09-01 21:45:51 -0400

    Remember to actually use the log_dir variable I set

--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -1,7 +1,7 @@
 class ChangeLog
-  directory = ENV['RAILS_ENV'] == 'production' ? "#{ENV['OPENSHIFT_LOG_DIR']}/cgbc_change.log" : "#{Rails.root}/log/change.log"
+  @log_dir = ENV['RAILS_ENV'] == 'production' ? "#{ENV['OPENSHIFT_LOG_DIR']}/cgbc_change.log" : "#{Rails.root}/log/change.log"
   def self.write(message)
-    @change_logger ||= Logger.new("#{Rails.root}/log/change.log")
+    @change_logger ||= Logger.new(@log_dir)
     @change_logger.info(message) unless message.nil?
   end
 end
