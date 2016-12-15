61f0bfb011ed1652be3f0c7a34096ccfaf515e4b
embell <emb1234@gmail.com>
2016-12-15 17:26:30 -0500

    Add skeleton for Sermon factory in spec

---
+++ spec/factories/sermons.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :sermon do
+    title "Test Sermon"
+    speaker "Marty Grainger"
+    date "2016-09-27"
+    # Need to add a file as well
+  end
+end
--- spec/models/sermon_spec.rb
+++ spec/models/sermon_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Sermon, type: :model do
   pending "Sermons search should find all sermons by speaker" 
+  # describe "self.search" do
+   #  it "finds correct results by speaker" do
+      
+    # end
+  # end
+
   pending "Sermons search should find all sermons by title" 
   pending "Sermons search should find all sermons by year" 
   pending "Sermons search should find all sermons by month" 
