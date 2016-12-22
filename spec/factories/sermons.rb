b9131115926abfd66acdbcc1f031461ca000ced9
embell <emb1234@gmail.com>
2016-12-22 14:59:27 -0500

    Add tests for Sermon.search
    These tests work correctly, but I need to add in a database cleaner to
    prevent repeated records.

--- spec/factories/sermons.rb
+++ spec/factories/sermons.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
   factory :sermon do
-    title "Test Sermon"
-    speaker "Marty Grainger"
+    title "Default"
+    speaker "Default"
     date "2016-09-27"
-    # Need to add a file as well
+
+    audio_file { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec", "fixtures", "test.txt")) }
+
+    trait :speaker_marty do
+      speaker "Marty Grainger"
+    end
+
+    trait :title_test_sermon do
+      title "Test Sermon"
+    end
   end
 end
--- spec/models/sermon_spec.rb
+++ spec/models/sermon_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Sermon, type: :model do
-  pending "Sermons search should find all sermons by speaker" 
-  # describe "self.search" do
-   #  it "finds correct results by speaker" do
-      
-    # end
-  # end
+  FactoryGirl.create(:sermon, :speaker_marty)
+  FactoryGirl.create(:sermon, :title_test_sermon)
+  
+  describe "self.search" do
+    it "finds correct results by title" do
+      results = Sermon.search({ title: "Test" })
+      expect(results.count).to eq(1)
+      results.each do |r|
+        expect(r.title).to include("Test")
+      end
+    end
+    
+    it "finds correct results by speaker" do
+      results = Sermon.search({ speaker: "Marty Grainger" })
+      expect(results.count).to eq(1)
+      results.each do |r|
+        expect(r.speaker).to include("Marty")
+      end
+    end
+  end
 
-  pending "Sermons search should find all sermons by title" 
   pending "Sermons search should find all sermons by year" 
   pending "Sermons search should find all sermons by month" 
   pending "Sermons search should find all sermons when combining all criteria" 
