84a26d5c543969890f1b6f4afaab3ed3f9afb2e7
embell <emb1234@gmail.com>
2017-02-14 16:46:00 -0500

    Add test for Event.upcoming

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,18 @@
 FactoryGirl.define do
   factory :event do
     name "MyString"
-    date "2016-09-27"
-    time "2016-09-27 11:30:54"
-    description "MyText"
+    date {Time.now}
+    time {Time.now}
+    description "This is a cool event."
+  end
+
+  trait :future do
+    date {100.days.from_now}
+    time {100.days.from_now}
+  end
+
+  trait :past do
+    date {100.days.ago}
+    time {100.days.ago}
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    FactoryGirl.create(:event, :past)
+    FactoryGirl.create(:event, :future)
+    FactoryGirl.create(:event, :past)
+  end
+
+  describe "self.upcoming" do
+    results = Event.upcoming()
+    
+    results.each do |r|
+      expect(r.date).to be > Time.now
+    end
+  end
 end
