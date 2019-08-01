87556c705600bec78d974ef8a9b1cb260c5fe68d
embell <emb1234@gmail.com>
2019-07-31 22:28:15 -0400

    Event seed data for dev environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 if Rails.env = 'development'
+  # Create sample users
   User.create(email: 'test@example.com', password: '12345678',
               password_confirmation: '12345678',
               permissions: User.all_permissions)
@@ -21,4 +22,11 @@ if Rails.env = 'development'
   User.create(email: 'news.events@example.com', password: '12345678',
               password_confirmation: '12345678',
               permissions: [:newsletters, :events])
+
+  # Sample events
+  Event.create(name: 'Sample event 1', start_date: Date.today + 3, 
+               description: 'This is the first test event.')
+  Event.create(name: 'Sample event 2', start_date: Date.today + 8)
+  Event.create(name: 'Sample event 3', start_date: Date.today + 11, 
+               end_date: Date.today + 16, description: 'This is another test event')
 end
