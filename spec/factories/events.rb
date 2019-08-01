bc1f8299437c9d2f2bd5018b7e47f38f6bd7e7a1
embell <emb1234@gmail.com>
2019-07-31 22:27:02 -0400

    Add another test for events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -30,4 +30,45 @@ FactoryBot.define do
     start_date {Date.yesterday}
     start_time {Date.yesterday}
   end
+
+  # Events in the current month, next and last year
+  trait :next_year do
+    start_date {Date.new(Date.today.year+1, Date.today.month)} 
+    start_time {Date.new(Date.today.year+1, Date.today.month)} 
+  end
+
+  trait :last_year do
+    start_date {Date.new(Date.today.year-1, Date.today.month)} 
+    start_time {Date.new(Date.today.year-1, Date.today.month)} 
+  end
+
+  # Events that span between months
+  trait :ends_next_month do
+    if (Date.today.month == 12)
+      next_month = 1
+      next_month_year = Date.today.year + 1
+    else
+      next_month = Date.today.month + 1
+      next_month_year = Date.today.year
+    end
+
+    start_date {Date.new(Date.today.year, Date.today.month, 26)}
+    start_time {Date.new(Date.today.year, Date.today.month, 26)}
+    end_date {Date.new(next_month_year, next_month, 7)}
+  end
+
+  trait :ends_this_month do
+    if (Date.today.month == 1)
+      last_month = 12
+      last_month_year = Date.today.year - 1
+    else
+      last_month = Date.today.month - 1
+      last_month_year = Date.today.year
+    end
+
+    start_date {Date.new(last_month_year, last_month, 28)}
+    start_time {Date.new(last_month_year, last_month, 28)}
+
+    end_date {Date.new(Date.today.year, Date.today.month, 3)}
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -48,7 +48,8 @@ RSpec.describe Event, type: :model do
   end
 
   describe "self.month_events" do
-    it "finds only events for the given month" do
+    # Test that no false positives are returned
+    it "finds events only for the given month" do
       results = Event.month_events(Date.today.year, Date.today.month)
 
       results.each do |e|
@@ -69,6 +70,30 @@ RSpec.describe Event, type: :model do
 
         expect(start_in_month || end_in_month).to be == true, "Date #{date_range_string} not in month #{Date.today.month} of #{Date.today.year}"
       end
+    end
+
+    # Test for false negatives
+    it "finds all events for a given month" do
+      results = Event.month_events(Date.today.year, Date.today.month)
+      all_events = Event.all
+
+      # if event is not in the results, make sure it shouldn't be
+      all_events.reject { |e| results.include?(e) }.each do |e| 
+        
+        start_in_month = (e.start_date.month == Date.today.month &&
+                          e.start_date.year == Date.today.year) 
+        end_in_month = !e.end_date.nil? && 
+                       (e.end_date.month == Date.today.month &&
+                        e.end_date.year == Date.today.year)
+
+        if e.end_date.nil? 
+          date_range_string = "#{e.start_date}"
+        else
+          date_range_string = "#{e.start_date} - #{e.end_date}"
+        end
+
+        expect(start_in_month || end_in_month).to be == false, "Event on #{date_range_string} should have been returned."
+     end
     end
   end
 
